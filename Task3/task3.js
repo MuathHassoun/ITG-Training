5bcfc9d352267bd655b974e3991b7ba7dc4213e2
Muath Hassoun <moath.hassoun@gmail.com>
2026-09-20 10:56:19 +0300

    complete task 1 and task 2

---
+++ Task3/task3.js
@@ -0,0 +1,8 @@
+var companies = {
+  "hardware": ["company1", "globalTech", "company2"],
+  "software": ["Asal", "InfiniteTiers", "Exhausted"],
+  "networking": ["company3", "company4", "company5"]
+};
+
+var bestCompany = companies.software[1];
+console.log("The best company in the software category is: " + bestCompany);
